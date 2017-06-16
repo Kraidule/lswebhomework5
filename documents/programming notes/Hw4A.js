4098cb667a27ded373433fad1f4bcf0d4bb839c2
Kraidule <kraidule@gmail.com>
2017-06-15 21:12:47 -0400

    I am learning how to use git at Lambda School's [online code bootcamp](https://lambdaschool.com/mini-bootcamp) I am learning how to use git at Lambda School's [online code bootcamp](https://lambdaschool.com/mini-bootcamp) I am learning how to use git at Lambda School's [online code bootcamp](https://lambdaschool.com/mini-bootcamp) I am learning how to use git at Lambda School's [online code bootcamp](https://lambdaschool.com/mini-bootcamp)

---
+++ documents/programming notes/Hw4A.js
@@ -0,0 +1,8 @@
+document.body.style.backgroundColor = "red";
+document.body.style.fontFamily = "sans-serif";
+document.querySelector("h1").style.backgroundColor = "blue";
+document.getElementById("nickname").innerHTML = "Ice";
+document.getElementById("favorites").innerHTML = "Programming, learning new things, cracking jokes";
+document.getElementById("hometown").innerHTML = "The Internet";
+ 
+ 
