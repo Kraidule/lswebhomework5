54fcadd1da90177c1690f1e7c1e49c9e0365f846
Kraidule <kraidule@gmail.com>
2017-06-22 15:09:01 -0400

    Removed bad files

--- documents/programming notes/Hw4A.js
+++
@@ -1,8 +0,0 @@
-document.body.style.backgroundColor = "red";
-document.body.style.fontFamily = "sans-serif";
-document.querySelector("h1").style.backgroundColor = "blue";
-document.getElementById("nickname").innerHTML = "Ice";
-document.getElementById("favorites").innerHTML = "Programming, learning new things, cracking jokes";
-document.getElementById("hometown").innerHTML = "The Internet";
- 
- 
